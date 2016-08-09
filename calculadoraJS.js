8a65b2dd3f33c5b0c41ace8cf28822f4c51ef5ce
diegocba <diegomcab123@hotmail.com>
2016-08-08 21:42:01 -0300

    Update calculadoraJS.js

--- calculadoraJS.js
+++ calculadoraJS.js
@@ -1,6 +1,13 @@
 function fx_calcular()
 {
 var result;
+var tipo_op;
+var nombre1;
+var nombre2;
+var valor1;
+var valor2;
+var nombre_op;
+var valor_op;
 
 nombre1= document.calculadora.input1.value; 
 nombre2= document.calculadora.input2.value;;
@@ -15,16 +22,21 @@ switch(valor_op)
 {
 	case "s":
 			result = parseInt(valor1) + parseInt(valor2);
+			tipo_op= "+";
 			break;
 	case "r":
 			result = valor1 - valor2;
+			tipo_op= "-";
 			break;
 	case "m":
 			result = valor1 * valor2;
+			tipo_op= "*";
 			break;
 	case "d":
 			result = valor1 / valor2;
+			tipo_op= "/";
 			break;
 }
-alert ("el resultado es: " + result);
+alert("operacion " + valor1 + tipo_op + valor2 + "=" + result);
+//alert ("el resultado es: " + result);
 }
